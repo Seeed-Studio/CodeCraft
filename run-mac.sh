df0155ea948b54b0571d7a43ece335dda76f8c70
王静茜 <jingxi.wang@seeed.cc>
2022-05-23 15:10:01 +0000

    Update run-mac.sh

--- run-mac.sh
+++ run-mac.sh
@@ -27,6 +27,6 @@ cd ${script_dir}/gui && rm -rf ../main/app/gui && cp -r ./build ../main/app/gui
 
 echo "======build app====="
 cd ${script_dir}/main/app && npm install -y --unsafe-perm=true
-cd ${script_dir}/main/app/node_modules/serialport/ && ../.bin/electron-rebuild
+cd ${script_dir}/main/app/node_modules/serialport/ && ../.bin/electron-rebuild serialport
 cd ${script_dir}/main && npm install -y --unsafe-perm=true && rm -rf build && npm run publish-mac
 
