57fa64b8acd3f0d80c9dddedf33bda3d087ad58f
王静茜 <jingxi.wang@seeed.cc>
2022-05-23 14:31:47 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,29 +5,28 @@ script_dir="/root/workspace/seeed-codecraft_2f88"
 export CSC_IDENTITY_AUTO_DISCOVERY=false
 export NODE_OPTIONS='--max_old_space_size=10240'
 
-RUN cd ${script_dir}/main && rm -rf build
+echo "======build gui-first time====="
+cd ${script_dir}/gui && npm install -y --unsafe-perm=true
 
-RUN echo "======build gui-first time====="
-RUN cd ${script_dir}/gui && npm install -y --unsafe-perm=true
+echo "======build blocks====="
+cd ${script_dir}/blocks && npm install -y --unsafe-perm=true
+cd ${script_dir}/blocks && npm run build-mac
 
-RUN echo "======build blocks====="
-RUN cd ${script_dir}/blocks && npm install -y --unsafe-perm=true
-RUN cd ${script_dir}/blocks && npm run build-mac
+echo "======build l10n====="
+cd ${script_dir}/l10n && npm install -y --unsafe-perm=true
+cd ${script_dir}/l10n && npm run build
 
-RUN echo "======build l10n====="
-RUN cd ${script_dir}/l10n && npm install -y --unsafe-perm=true
-RUN cd ${script_dir}/l10n && npm run build
+echo "======build vm====="
+cd ${script_dir}/vm && npm install -y --unsafe-perm=true
+cd ${script_dir}/vm && npm run build
 
-RUN echo "======build vm====="
-RUN cd ${script_dir}/vm && npm install -y --unsafe-perm=true
-RUN cd ${script_dir}/vm && npm run build
+echo "======build gui-second time====="
+cd ${script_dir}/gui && npm install -y --unsafe-perm=true
+cd ${script_dir}/gui && npm run build-mac
+cd ${script_dir}/gui && rm -rf ../main/app/gui && cp -r ./build ../main/app/gui
 
-RUN echo "======build gui-second time====="
-RUN cd ${script_dir}/gui && npm install -y --unsafe-perm=true
-RUN cd ${script_dir}/gui && npm run build-mac
-RUN cd ${script_dir}/gui && rm -rf ../main/app/gui && cp -r ./build ../main/app/gui
+echo "======build app====="
+cd ${script_dir}/main/app && npm install -y --unsafe-perm=true
+cd ${script_dir}/main/app/node_modules/serialport/ && ../.bin/electron-rebuild
+cd ${script_dir}/main && npm install -y --unsafe-perm=true && rm -rf build && npm run publish-mac
 
-RUN echo "======build app====="
-RUN cd ${script_dir}/main/app && npm install -y --unsafe-perm=true
-RUN cd ${script_dir}/main/app/node_modules/serialport/ && ../.bin/electron-rebuild
-RUN cd ${script_dir}/main && npm install -y --unsafe-perm=true && rm -rf build && npm run publish-mac
