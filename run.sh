f094153a1370ba2baf83a968c319cdbd248a7dc6
王静茜 <jingxi.wang@seeed.cc>
2022-05-23 14:25:12 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,6 +5,8 @@ script_dir="/root/workspace/seeed-codecraft_2f88"
 export CSC_IDENTITY_AUTO_DISCOVERY=false
 export NODE_OPTIONS='--max_old_space_size=10240'
 
+RUN cd ${script_dir}/main && rm -rf build
+
 RUN echo "======build gui-first time====="
 RUN cd ${script_dir}/gui && npm install -y --unsafe-perm=true
 
